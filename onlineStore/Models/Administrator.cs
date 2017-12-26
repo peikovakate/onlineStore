e8a8c06957253c88403bb0406753d837624252a4
Kateryna Peikova <peikovakate@gmail.com>
2017-12-26 12:37:31 +0200

    added password props to Administrator class

--- onlineStore/Models/Administrator.cs
+++ onlineStore/Models/Administrator.cs
@@ -11,6 +11,8 @@ namespace onlineStore.Models
 
         public string Username { get; set; }
 
+        public byte[] PasswordHash { get; set; }
 
+        public byte[] PasswordSalt { get; set; }
     }
 }
