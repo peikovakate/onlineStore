2c9b345b0082503a08682961c809cb8729fae9f8
Kateryna Peikova <peikovakate@gmail.com>
2017-12-26 13:28:13 +0200

    added migration of adding IsConfirmed column to Administrator table

---
+++ onlineStore/Migrations/20171226112004_Administrators_IsConfirmed_prop.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace onlineStore.Migrations
+{
+    public partial class Administrators_IsConfirmed_prop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsConfirmed",
+                table: "Administrators",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "Administrators");
+        }
+    }
+}
