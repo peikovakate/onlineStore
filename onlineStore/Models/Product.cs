6b259b0876ecff3298ce458c96a4201054585047
Kateryna Peikova <peikovakate@gmail.com>
2017-12-26 12:35:42 +0200

    added some props to Product class

--- onlineStore/Models/Product.cs
+++ onlineStore/Models/Product.cs
@@ -7,5 +7,12 @@ namespace onlineStore.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
     }
 }
