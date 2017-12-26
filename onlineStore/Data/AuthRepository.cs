0fba5b747d69d8da15023125048c87335582bb1d
Kateryna Peikova <peikovakate@gmail.com>
2017-12-26 13:51:47 +0200

    added some logic to AuthRepository class

--- onlineStore/Data/AuthRepository.cs
+++ onlineStore/Data/AuthRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using onlineStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace onlineStore.Data
 {
@@ -10,19 +12,61 @@ namespace onlineStore.Data
         {
             _context = context;
         }
-        public Task<bool> AdministratorExists(string username)
+        public async Task<bool> AdministratorExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Administrators.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
         }
 
-        public Task<Administrator> Login(string username, string password)
+        public async Task<Administrator> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var admin = await _context.Administrators.FirstOrDefaultAsync(x => x.Username == username);
+            if (admin == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, admin.PasswordHash, admin.PasswordSalt))
+                return null;
+
+            //auth succsessful
+            return admin;
+
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i=0; i<computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+
+                }
+            }
+            return true;
         }
 
         public async Task<Administrator> Register(Administrator admin, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            admin.PasswordHash = passwordHash;
+            admin.PasswordSalt = passwordSalt;
+
+            await _context.AddAsync(admin);
+            await _context.SaveChangesAsync();
+            return admin;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
--- onlineStore/Startup.cs
+++ onlineStore/Startup.cs
@@ -28,6 +28,7 @@ namespace onlineStore
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
